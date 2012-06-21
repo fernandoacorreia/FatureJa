40bfefd701a889e03ebda990a971535b265b22cb
Fernando Correia <fernandoacorreia@gmail.com>
2012-06-21 16:40:40 -0300

    Otimizações

--- FatureJa.Worker/WorkerRole.cs
+++ FatureJa.Worker/WorkerRole.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Net;
 using FatureJa.Negocio.Infraestrutura;
@@ -18,11 +18,20 @@ namespace FatureJa.Worker
 
         public override bool OnStart()
         {
-            Diagnostico.Configurar();
-            Trace.WriteLine("Executando base.OnStart() de WorkerRole.");
+            // Desativar Nagle para melhorar a performance
+            // http://blogs.msdn.com/b/windowsazurestorage/archive/2010/06/25/nagle-s-algorithm-is-not-friendly-towards-small-requests.aspx
+            ServicePointManager.UseNagleAlgorithm = false;
 
             // Set the maximum number of concurrent connections
+            // http://social.technet.microsoft.com/Forums/en-US/windowsazuredata/thread/d84ba34b-b0e0-4961-a167-bbe7618beb83
             ServicePointManager.DefaultConnectionLimit = 100;
+
+            // Turn off 100-continue (saves 1 roundtrip)
+            ServicePointManager.Expect100Continue = false;
+
+            // Configurar diagnóstico
+            Diagnostico.Configurar();
+            Trace.WriteLine("Executando base.OnStart() de WorkerRole.");
 
             // For information on handling configuration changes
             // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
